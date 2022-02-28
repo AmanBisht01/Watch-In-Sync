5398e66100b01e744ab6e8276cebb4290511482a
AmanBisht01 <bishtaman1111@gmail.com>
2022-02-28 11:38:27 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from "./Home.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Room from "./Room.js";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="room=/:roomid" element={<Room />} />
+        <Route path="/room" element={<Room />} />
+
+        <Route path="/join" element={<Room />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
